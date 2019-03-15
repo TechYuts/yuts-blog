e3a16256b1e074eb1cc304dc1c55e91f64b74092
yuta yanagisawa <yuta.yanagisawa@soliton.co.jp>
2019-03-15 16:29:02 +0900

    add slug support

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,4 +1,20 @@
 'use strict';
 
+const path = require("path")
+const { createFilePath, createFileNode } = require(`gatsby-source-filesystem`);
+
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        const slug = createFilePath({ node, getNode, basePath: `pages` })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: slug,
+        })
+
+    }
+}
